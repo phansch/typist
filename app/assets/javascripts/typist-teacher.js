de1301921445cce8ff8982779ae69bfd0396f43c
Philipp Hansch <dev@phansch.net>
2013-11-20 11:14:33 +0100

    Add user controller

--- app/assets/javascripts/typist-teacher.js
+++ app/assets/javascripts/typist-teacher.js
@@ -1,7 +1,14 @@
 
 // JS for typist
 
-$(document).ready(function () {
+$(function() {
+  initPage();
+});
+$(window).bind('page:change', function() {
+  initPage();
+});
+function initPage() {
+	//TODO: Get this from a rest api
 	var text = 'iiii dddd iiii dddd iiii idi didi id'
 	var arr = text.split(' ')
 
@@ -20,11 +27,36 @@ $(document).ready(function () {
 				return false;
 			}
 		} else {
+			highlight_mistake()
 			// Set input background to red
 		}
 
 	});
+
+}
+
+$(function() {
+    $('#typist')
+        .keyup(checkSpeed);
 });
+var iLastTime = 0;
+var iTime = 0;
+var iTotal = 0;
+var iKeys = 0;
+
+function checkSpeed() {
+    iTime = new Date().getTime();
+
+    if (iLastTime != 0) {
+        iKeys++;
+        iTotal += iTime - iLastTime;
+        iWords = $('#typist').val().split(/\s/).length;
+        $('#CPM').html(Math.round(iKeys / iTotal * 6000, 2));
+        $('#WPM').html(Math.round(iWords / iTotal * 6000, 2));
+    }
+
+    iLastTime = iTime;
+}
 
 function highlight_mistake() {
 	// body...
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Typist::Application.routes.draw do
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/learn', to: 'static_pages#learn', via: 'get'

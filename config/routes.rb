73f3691025420410e404d5f7e0dc4ee49c1f7da2
Philipp Hansch <dev@phansch.net>
2013-12-03 13:17:26 +0100

    Niceyfi routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Typist::Application.routes.draw do
+  # General resources
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  # Lesson routes
+  match 'lessons/:id',  to: 'lesson#show',      via: 'get'
+
+  # Practice routes
+  match 'practice/:id', to: 'practice#show',    via: 'get'
+  match 'practice',     to: 'practice#create',  via: 'post'
+
+  # Static pages
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/learn',   to: 'static_pages#learn',   via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   root 'static_pages#home'
-
 end

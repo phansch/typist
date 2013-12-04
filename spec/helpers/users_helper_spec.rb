bdc034f1370dd340d9c64e8be0fc3c5d3271faf0
Philipp Hansch <dev@phansch.net>
2013-12-04 13:14:24 +0100

    Tests for users_helper

---
+++ spec/helpers/users_helper_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe UsersHelper do
+
+  before do
+    @user = FactoryGirl.create(:user)
+  end
+
+  describe "#gravatar_for" do
+    it "should return the gravatar image for the given user" do
+      expect(gravatar_for(@user)).to eq(gravatar_img)
+    end
+  end
+
+  describe "#avarage_wpm_for" do
+    before do
+      FactoryGirl.create_list(:practice, 10, wpm: 15)
+    end
+
+    it "should return the avarage wpm for the last 10 practices" do
+      expect(avarage_wpm_for(@user)).to eq 15
+    end
+  end
+end
+
+
+private
+
+  def gravatar_img
+    gravatar_id = Digest::MD5::hexdigest(@user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    img = "<img alt=\"#{@user.name}\" class=\"gravatar\" src=\"#{gravatar_url}\" />"
+  end

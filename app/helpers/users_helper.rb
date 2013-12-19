6364f84801e233e5d1bdfcd5020503027e33dbe1
Philipp Hansch <dev@phansch.net>
2013-12-19 14:30:59 +0100

    Increase gravatar size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,8 @@ module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}&s=400"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar img-rounded", width: 220)
   end
 
   def average_wpm_for(user)

b9b670774e04f26fe54d0f382255818817d9c897
Philipp Hansch <dev@phansch.net>
2014-02-26 15:31:26 +0100

    Add basic backbone structure

---
+++ app/assets/javascripts/typist.js
@@ -0,0 +1,13 @@
+window.Typist = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Typist.initialize();
+});
